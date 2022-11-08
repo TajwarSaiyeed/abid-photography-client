5719abcc8969bdc0cee0acec013892c00d458de0
TajwrSaiyeed <rjabid36@gmail.com>
2022-11-08 21:50:03 +0600

    upload system added

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,17 +1,50 @@
 import { GoogleAuthProvider } from "firebase/auth";
-import React, { useContext } from "react";
+import { getDownloadURL, listAll, ref, uploadBytes } from "firebase/storage";
+import React, { useContext, useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import { FcGoogle } from "react-icons/fc";
 import { Link, Navigate, useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/AuthProvider/AuthProvider";
+import { storage } from "../../firebase/firebase.config";
+import { v4 } from "uuid";
 
 const provider = new GoogleAuthProvider();
 const Signup = () => {
+  const [image, setImage] = useState(null);
+  const [imgurl, setImgurl] = useState(null);
+  const [downloadUrl, setDownloadUrl] = useState(null);
+  const imageListRef = ref(storage, "images/");
   const { googleLoginSignin, update, createUserWithEmailPassword, user } =
     useContext(AuthContext);
   const navigate = useNavigate();
   const location = useLocation();
   const from = location.state?.from?.pathname || "/";
+
+  const upload = () => {
+    // const image = e.target.files[0];
+    if (image === null) return;
+
+    const imageSplit = `images/${image.name + v4()}`.split(" ").join("");
+
+    const imageRef = ref(storage, imageSplit);
+    // console.log("imageRef : ", imageRef);
+    uploadBytes(imageRef, image).then(() => {
+      toast("image uploaded");
+      setImgurl(imageSplit);
+    });
+  };
+  useEffect(() => {
+    // setDownloadUrl(null);
+    listAll(imageListRef).then((res) => {
+      const im = res.items.find((i) => i._location.path_ === imgurl);
+      if (im) {
+        getDownloadURL(im).then((url) => {
+          setDownloadUrl(url);
+        });
+      }
+    });
+  }, [imgurl, imageListRef, downloadUrl]);
+
   const handleGoogleSignup = () => {
     googleLoginSignin(provider)
       .then(() => {
@@ -30,6 +63,7 @@ const Signup = () => {
 
     const namephone = {
       displayName: name,
+      photoURL: downloadUrl,
     };
 
     createUserWithEmailPassword(email, password)
@@ -47,7 +81,10 @@ const Signup = () => {
     return <Navigate to="/"></Navigate>;
   }
   return (
-    <div className="min-h-screen my-5 flex flex-col items-center justify-center bg-slate-200 rounded-2xl">
+    <div className="min-h-screen py-5 my-5 flex flex-col items-center justify-center bg-slate-200 rounded-2xl">
+      <div className="w-50 h-50 rounded-full overflow-hidden">
+        <img src={downloadUrl} className="w-full h-full rounded-full" alt="" />
+      </div>
       <h1 className="font-bold text-2xl my-5 uppercase">
         Sign up to Abid PhotoGraphy
       </h1>
@@ -58,6 +95,30 @@ const Signup = () => {
         <FcGoogle className="bg-white rounded-full p-1 w-6 h-6" /> &nbsp; Sign
         up with Google
       </button>
+
+      <div
+        className={
+          !downloadUrl
+            ? "bg-red-500 my-5 form-control border-black border p-5 rounded-xl w-2/4"
+            : "form-control my-5 border-black border p-5 rounded-xl w-2/4"
+        }
+      >
+        <label className="label">
+          <span className="label-text font-bold">User Image</span>
+        </label>
+        <input
+          type="file"
+          name="file"
+          accept="image/*"
+          onChange={(e) => setImage(e.target.files[0])}
+          className="file-input file-input-ghost w-full"
+          required
+        />
+        <br />
+        <button className="btn" onClick={upload}>
+          Upload
+        </button>
+      </div>
 
       <form
         onSubmit={handleUserSignup}
@@ -70,11 +131,24 @@ const Signup = () => {
           <input
             type="text"
             name="name"
+            required
             placeholder="Type here"
             className="input input-bordered w-full"
           />
         </div>
 
+        <div className="form-control w-full ">
+          <label className="label">
+            <span className="label-text font-bold">Image Url</span>
+          </label>
+          <input
+            type="text"
+            readOnly
+            required
+            defaultValue={downloadUrl}
+            className="input input-bordered w-full"
+          />
+        </div>
         <div className="form-control w-full ">
           <label className="label">
             <span className="label-text font-bold">Email</span>
@@ -99,7 +173,9 @@ const Signup = () => {
         </div>
         <input
           type="submit"
-          className="btn btn-outline btn-accent my-5 w-96 "
+          className={`${
+            !downloadUrl && "btn-disabled"
+          } btn btn-outline btn-accent my-5 w-96`}
           value="register"
         />
         <div className="font-semibold text-xl">
