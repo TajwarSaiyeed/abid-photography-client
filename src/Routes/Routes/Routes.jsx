8e883199196f7b020f9d131397e52ea3cc103290
TajwrSaiyeed <rjabid36@gmail.com>
2022-11-08 09:59:28 +0600

    login page added and designed

--- src/Routes/Routes/Routes.jsx
+++ src/Routes/Routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layout/Main/Main";
 import Home from "../../pages/Home/Home";
+import Login from "../../pages/Login/Login";
 import NotFound from "../../pages/NotFound/NotFound";
 import Services from "../../pages/Services/Services";
 import Signup from "../../pages/Signup/Signup";
@@ -23,6 +24,10 @@ export const routes = createBrowserRouter([
       {
         path: "/signup",
         element: <Signup />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
       },
     ],
   },
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,28 +1,18 @@
 import React from "react";
 import { FcGoogle } from "react-icons/fc";
 import { Link } from "react-router-dom";
-const Signup = () => {
+const Login = () => {
   return (
     <div className="min-h-screen my-5 flex flex-col items-center justify-center bg-slate-200 rounded-2xl">
       <h1 className="font-bold text-2xl my-5 uppercase">
-        Sign up to Abid PhotoGraphy
+        Login to Abid PhotoGraphy
       </h1>
       <button className="btn bg-blue-600 border-none">
         <FcGoogle className="bg-white rounded-full p-1 w-6 h-6" /> &nbsp; Sign
-        up with Google
+        in with Google
       </button>
 
       <form className="w-2/4 flex flex-col justify-center items-center">
-        <div className="form-control w-full">
-          <label className="label">
-            <span className="label-text font-bold">Name</span>
-          </label>
-          <input
-            type="text"
-            placeholder="Type here"
-            className="input input-bordered w-full"
-          />
-        </div>
         <div className="form-control w-full ">
           <label className="label">
             <span className="label-text font-bold">Email</span>
@@ -45,13 +35,13 @@ const Signup = () => {
         </div>
         <input
           type="submit"
-          className="btn btn-outline btn-accent my-5 w-96 "
+          className="btn btn-outline btn-accent my-5 w-96"
           value="register"
         />
         <div className="font-semibold text-xl">
-          Already Have An Account?{" "}
-          <Link className="link link-neutral" to="/login">
-            Login
+          Dont't Have An Account?{" "}
+          <Link className="link link-neutral" to="/signup">
+            Signup
           </Link>
         </div>
       </form>
@@ -59,4 +49,4 @@ const Signup = () => {
   );
 };
 
-export default Signup;
+export default Login;
