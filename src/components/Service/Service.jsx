5acdd0c9b4cbe0d9e9cc304ff1e41844cac92d3a
TajwrSaiyeed <rjabid36@gmail.com>
2022-11-09 10:10:11 +0600

    react photo view added to service pic complete

--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { PhotoProvider, PhotoView } from "react-photo-view";
 
 const Service = ({ service }) => {
   // console.log(service);
   const { _id, name, picture, description, price } = service;
   return (
     <div className="card card-side bg-base-100 shadow-xl">
-      <figure>
-        <img className="w-52 lg:w-80 h-full" src={picture} alt="Movie" />
-      </figure>
+      <PhotoProvider>
+        <PhotoView src={picture}>
+          <figure>
+            <img className="w-52 lg:w-80 h-full" src={picture} alt="Movie" />
+          </figure>
+        </PhotoView>
+      </PhotoProvider>
       <div className="card-body w-2/4 lg:w-full">
         <h2 className="card-title">{name}</h2>
         <p>{description.length > 100 && description.slice(0, 100) + "..."}</p>
