5c190b64a3ca4d072b4e37197588088244d5b9e4
TajwrSaiyeed <rjabid36@gmail.com>
2022-11-09 10:26:09 +0600

    if no review show message you have no review yet added

--- src/pages/MyReview/MyReview.jsx
+++ src/pages/MyReview/MyReview.jsx
@@ -29,43 +29,61 @@ const MyReview = () => {
       });
   };
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-3 gap-3 my-5">
-      {myreviews.map((myreview) => (
-        <div
-          key={myreview._id}
-          className="card max-w-96 bg-base-100 shadow-xl image-full"
-        >
-          <figure>
-            <img className="w-full" src={myreview.servicePicture} alt="Shoes" />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">{myreview.serviceTitle}</h2>
-            <p>{myreview.reviewMessage}</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Buy Now</button>
-              <button
-                onClick={() => handleDelete(myreview._id)}
-                className="btn btn-error btn-square"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
+    <div
+      className={`${
+        myreviews.length > 0
+          ? "lg:grid-cols-3"
+          : "justify-items-center lg:grid-cols-1"
+      } grid grid-cols-1 gap-3 my-5 `}
+    >
+      {myreviews.length > 0 ? (
+        myreviews.map((myreview) => (
+          <div
+            key={myreview._id}
+            className="card min-w-96 bg-base-100 shadow-xl image-full"
+          >
+            <figure>
+              <img
+                className="w-full"
+                src={myreview.servicePicture}
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{myreview.serviceTitle}</h2>
+              <p>{myreview.reviewMessage}</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+                <button
+                  onClick={() => handleDelete(myreview._id)}
+                  className="btn btn-error btn-square"
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
             </div>
           </div>
+        ))
+      ) : (
+        <div className="flex justify-center items-center">
+          <div className="bg-red-600 text-white p-3 text-center text-3xl font-bold rounded-lg">
+            No reviews were added
+          </div>
         </div>
-      ))}
+      )}
     </div>
   );
 };
